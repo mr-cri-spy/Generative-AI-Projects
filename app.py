857c98ce6c886f721f530462337af9292b733cf8
mr-cri-spy <kiraj8899@gmail.com>
2025-05-15 08:29:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import streamlit as st
 import google.generativeai as genai 
 
-load_dotenv()   ## load all the variables that is defined in .env file
+load_dotenv()   ##load all the variables that is defined in .env file
 
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))  ## configure the API key
 
